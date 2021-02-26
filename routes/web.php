7b22c1821399c1685b33f35acaf2abf5196632e4
Woles275 <75599380+Woles275@users.noreply.github.com>
2021-02-26 11:54:51 +0700

    master

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Practicum 1
+//number 1.
+Route::get('/contact', function () {return view('contact'); })->name('contact');
+Route::get('/gallery', function () {return view('gallery'); })->name('gallery');
+//number 2.
+Route::get('/', function () {
+    echo "Welcome";
+});
+//number 3.
+Route::get('/about', function () {
+    echo "Name: Della Arselatuiqrom NIM: 1941720027 Class: TI-2H";
+});
+
+Route::get('/article/{id}', function ($id) {
+    echo ("This is article with id ".$id);
+});
+
+//Practicum 2
+//number 1.
+//number 2.
+//number 3.
+//Practicum 3
+//number 1.
+//number 2.
+//number 3.
