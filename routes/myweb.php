4ea0a0dd7276049dc57a1cb7d3e75961d75956f7
Woles275 <75599380+Woles275@users.noreply.github.com>
2021-03-05 18:38:40 +0700

    change home 8000

---
+++ routes/myweb.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//Practicum 1 :
+//number 1.
+Route::get('/contact', function () {return view('contact'); })->name('contact');
+Route::get('/gallery', function () {return view('gallery'); })->name('gallery');
+//number 2.
+Route::get('/', function () {
+    echo "Welcome";
+});
+//number 3.
+//Route::get('/about', function () {
+    //echo "Name: Della Arselatuiqrom NIM: 1941720027 Class: TI-2H";
+//});
+
+//Route::get('/article/{id}', function ($id) {
+   // echo ("This is article with id ".$id);
+//});
+
+//Practicum 2 :
+//number 2.
+//Route::get('/', [PageController::class, 'index']);
+
+//Route::get('/about', [PageController::class, 'about']);
+
+//Route::get('articles/{id}', [PageController::class, 'articles']);
+
+//number 3.
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('articles/{id}', [ArticleController::class, 'articles']);
+
+//Practicum 3
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
