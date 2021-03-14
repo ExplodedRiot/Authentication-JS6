b8cfeff22ace75fee548fb570baeba3d83654af5
ExplodedRiot <alive_ananda@yahoo.com>
2021-03-14 16:08:13 +0700

    modify seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -13,6 +13,12 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('posts')->insert([
+            'title' => 'Belajar Model Dengan Laravel',
+            'slug' => ' belajar-model-dengan-laravel',
+            'image' => 'https://via.placeholder.com/640x480.png/00ff77?text=Belajar Model',
+            'content' => 'Belajar Laravel itu menyenangkan',
+            'draft' => 0
+            ]);
     }
 }
